8f3d511d8d5991d4fc1f78207b783ed654116928
Pothu-Abhilash <abhipothu2000@gmail.com>
2024-04-13 14:15:19 +0530

    First

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,9 +9,19 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.type= type;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear =currentGear;
+        this.seats = seats;
     }
+
 
     public void changeGear(int newGear){
 
@@ -21,5 +31,61 @@ public class Car extends Vehicle {
     public void changeSpeed(int newSpeed, int newDirection){
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+    public int getCurrentGear(int currentGear)
+    {
+        return currentGear;
+    }
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
     }
 }
